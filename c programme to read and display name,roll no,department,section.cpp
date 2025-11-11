7e9c73df1c2f1ac4bec85d7589577eca2c225efa
mohammadabidha98-png <mohammadabidha98@gmail.com>
2025-11-11 21:50:01 +0530

    Add files via upload

---
+++ c programme to read and display name,roll no,department,section.cpp
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	char name[10],rollnumber[10],department[4];
+	int section;
+	puts("enter your name:");
+	gets(name);
+	puts("your name is:");
+	puts(name);
+	puts("enter rollnumber:");
+	gets(rollnumber);
+	puts("enter department");
+	gets(department);
+	printf("enter section:");
+	scanf("%d",&section);
+	return 0;
+	
+}
