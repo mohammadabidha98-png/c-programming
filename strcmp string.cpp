d48578dcbf81cd1ff0a913f5944243abb7bf1df9
mohammadabidha98-png <mohammadabidha98@gmail.com>
2025-11-12 15:14:52 +0530

    Add files via upload

---
+++ strcmp string.cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+	char s1[10]="ABCD";
+	char s2[10]="ABCE";
+	int res=strcmp(s1,s2);
+	if(res==0)
+	printf("two strings are equal");
+	else if(res<0)
+	printf("%s is smaller than %s",s1,s2);
+	else
+	printf("%s is larger than %s",s1,s2);
+	return 0;
+}
